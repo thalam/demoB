6d1c84b94abb030e5869d198789b2f7ddda6800c
Jane Aarthy Joseph <jjose139@my.centennialcollege.ca>
2022-07-25 07:55:16 -0400

    First commit

---
+++ src/main/java/com/example/serverB/demoB/DemoBApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverB.demoB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serverB/demoB/DemoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverB.demoB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
