cf18dd0986dbf06f3cbc5b41251976f5797ce339
Jane Aarthy Joseph <jjose139@my.centennialcollege.ca>
2022-07-25 08:53:03 -0400

    hello

--- src/main/java/com/example/serverB/demoB/DemoBApplication.java
+++ src/main/java/com/example/serverB/demoB/DemoBApplication.java
@@ -1,13 +1,10 @@
 package com.example.serverB.demoB;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DemoBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoBApplication.class, args);
+		System.out.println(">>> B");
 	}
 
 }
--- src/test/java/com/example/serverB/demoB/DemoBApplicationTests.java
+++ src/test/java/com/example/serverB/demoB/DemoBApplicationTests.java
@@ -1,12 +1,8 @@
 package com.example.serverB.demoB;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoBApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
